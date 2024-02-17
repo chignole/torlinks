87b3d13be66ea668c611849bb3e36b32de48771c
Raoul Hernandez <raoul.hernandez1@gmail.com>
2024-02-17 21:07:27 +0000

    update

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
-	"strings"
 
 	"github.com/j-muller/go-torrent-parser"
 )
 
 // TODO Dissociate failure and success in finding files for symlinks
+// TODO Use viper for confguration
 
 // Variables
 var torrentFolder string
@@ -29,12 +30,22 @@ type file struct {
 	showEpisode string
 }
 
+type configuration struct {
+	General general `json:"general"`
+}
+
+type general struct {
+	Source  string `json:"source"`
+	TvShows string `json:"tvshows"`
+	Movies  string `json:"movies"`
+}
+
 // Search for specific file extension in a directory - Returning an array of files
 func find(root, ext string) []string {
 	var a []string
 	filepath.WalkDir(root, func(s string, d fs.DirEntry, e error) error {
 		if e != nil {
-			return e
+			log.Fatalf("[ERROR] Error while looking for torrent files : %v", e)
 		}
 		if filepath.Ext(d.Name()) == ext {
 			a = append(a, s)
@@ -47,10 +58,10 @@ func find(root, ext string) []string {
 // Parse a torrent - Returns an array of file structs
 func parse(torrent string) []file {
 	var parsedFiles []file
-	a, err := gotorrentparser.ParseFromFile(torrent)
 	showPattern := regexp.MustCompile(`(.*)(?:\.S\d{2}.*)(S\d{2}E\d{2})`)
+	a, err := gotorrentparser.ParseFromFile(torrent)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("[ERROR] Error while parsing torrent : %v", err)
 	} else {
 		for _, f := range a.Files {
 			var showTitle string = "none"
@@ -85,58 +96,58 @@ func createSymlink(f file, showFolder string) bool {
 		if e != nil {
 			log.Fatalf("[ERROR] Error while browsing media directory : %v", e)
 		}
-		if mediaFolder == showFolder {
-			if strings.Contains(a, f.showTitle) {
-				fileStats, err := os.Stat(a)
-				if err != nil {
-					log.Fatalf("[ERROR] Error while getting filestats : %v", err)
-				}
-				if fileStats.Size() == f.size {
-					// fmt.Println(f.path, "->", a)
-					if folder != nil {
-						os.Mkdir(folder[0], 0744)
-					}
-					os.Symlink(a, f.path)
-					log.Println("[PASS]", a, f.path)
-					symLink = true
-				}
-			}
+		fileStats, err := os.Stat(a)
+		if err != nil {
+			log.Fatalf("[ERROR] Error while getting filestats : %v", err)
 		}
-		if mediaFolder == movieFolder {
-			fileStats, err := os.Stat(a)
-			if err != nil {
-				log.Fatalf("[ERROR] Error while getting filestats : %v", err)
+		if fileStats.Size() == f.size {
+			if folder != nil {
+				os.Mkdir(folder[0], 0744)
 			}
-			if fileStats.Size() == f.size {
-				// fmt.Println(f.path, "->", a)
-				if folder != nil {
-					os.Mkdir(folder[0], 0744)
-				}
-				os.Symlink(a, f.path)
-				log.Println("[PASS]", a, f.path)
-				symLink = true
-			}
+			os.Symlink(a, f.path)
+			log.Println("[PASS]", a, f.path)
+			symLink = true
 		}
 		return nil
 	})
 	return symLink
 }
 
-func main() {
+// Processing configuration file
+func initConfig() configuration {
+	configFile, err := os.Open("./config/config.json")
+	if err != nil {
+		log.Fatalf("[ERROR] Error opening configuration file : %v", err)
+	}
+	defer configFile.Close()
+	config, err := ioutil.ReadAll(configFile)
+	if err != nil {
+		log.Panicf("[ERROR] Error processing configuration file : %v", err)
+	}
+	loadConfig := configuration{}
+	err = json.Unmarshal(config, &loadConfig)
+	if err != nil {
+		log.Panicf("[ERROR] Error processing configuration file : %v", err)
+	}
+	return loadConfig
+}
 
+// Main function
+func main() {
 	// Logging setup
 	f, err := os.OpenFile("torlinks.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+		log.Fatalf("[ERROR] Error opening file: %v", err)
 	}
 	defer f.Close()
 	log.SetOutput(f)
 
-	// Parse flags configuration
-	flag.StringVar(&torrentFolder, "s", "./", "folder containing torrent files to process")
-	flag.StringVar(&showFolder, "d", "./", "folder containing tv show files")
-	flag.StringVar(&movieFolder, "m", "./", "folder containing movie files")
-	flag.Parse()
+	// Loading configuration file
+	loadConfig := initConfig()
+	fmt.Println(loadConfig.General.Movies)
+	torrentFolder = loadConfig.General.Source
+	movieFolder = loadConfig.General.Movies
+	showFolder = loadConfig.General.TvShows
 
 	// Search for torrent files in specified folder
 	torrents := find(torrentFolder, ".torrent")
